dbaf771434de27f89686412fa2c39d9acc686d13
BenMeie <crazycowmm@gmail.com>
2024-08-29 19:17:06 -0700

    bump package.json

--- dist/utils.js
+++ dist/utils.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.isSlotId = exports.isPublisherId = void 0;
-const PUBLISHER_ID_REGEX = /^pub-\d{16}$/;
+const PUBLISHER_ID_REGEX = /^(ca-)?pub-\d{16}$/;
 const SLOT_ID_REGEX = /^\d{10}$/;
 function isPublisherId(id) {
     if (typeof id !== "string") {
